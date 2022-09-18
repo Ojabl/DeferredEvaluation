a907e1080c123f87049f128b2e4ff5607819a973
osajab10@gmail.com <osajab10@gmail.com>
2022-09-18 18:19:18 +0200

    Add project files.

---
+++ DeferredEvaluation/PrintWhenGetting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeferredEvaluation
+{
+    class PrintWhenGetting
+    {
+        private int instanceNumber;
+        public int InstanceNumber
+        {
+            get
+            {
+                Console.WriteLine($"Getting #{instanceNumber}");
+                return instanceNumber;
+            }
+            set { instanceNumber = value; }
+        }
+    }
+}
